3b39bd28802cb6f90d865797276b2d77fa5d73fd
Vicki Chen <vchen@ucsd.edu>
2021-02-25 11:33:58 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'sSMUIsubnRYZpImv9Uz7yCrIWXA1x0i7';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.8785, -117.2359]).addTo(map);
 }
